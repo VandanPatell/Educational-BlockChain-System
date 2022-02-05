b4d08e8d0e4254fc1a929f24b343b82dbb984458
Vandan Patel <vandanp89@gmail.com>
2022-02-05 12:01:02 +0530

    testing demo code

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import json
 from hashlib import sha256 as sha
+import time
+from flask import Flask, request
+import requests
 # Class Block will contain information about the block
-# index - index of the block in blockchain
-# data - Data that is to be stored on blockchain
+# index - index of the block in blockChain
+# data - Data that is to be stored on blockChain
 # Previous Hash - Previous hash of the block
 # Nonce - key that makes block unique
+
+
 class Block:
-    ''' Class Block creats the object of block that is to added to blockchain. '''
+    ''' Class Block creats the object of block that is to added to blockChain. '''
+
     def __init__(self, index, data, timestamp, previous_hash, nonce=0):
         self.index = index
         self.data = data
@@ -16,9 +22,80 @@ class Block:
 
     def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True)
-        return sha256(block_string.encode()).hexdigest()
+        return sha(block_string.encode()).hexdigest()
 
 
-B = Block(0,"Hello World","10:22","ASBASDKG",10)
+class BlockChain:
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
 
-print(B.__dict__)
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    difficulty = 2
+
+    def proof_of_work(self, block):
+        # block.nonce =
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * BlockChain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+        return computed_hash
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+        if previous_hash != block.previous_hash:
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * BlockChain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+
+app = Flask(__name__)
+
+blockchain = BlockChain()
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+
+
+app.run(debug=True, port=5000)
